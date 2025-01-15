3a6e784031e64c63b465f95bd45d2cda58f5c4d3
Bohdan <bohdan_voroniak@epam.com>
2025-01-15 17:08:08 +0200

    init project

---
+++ Offers/Program.cs
@@ -0,0 +1,18 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
