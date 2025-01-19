9bce7fb907d6e58a694e621273a682b1f5a800f4
Bohdan <bohdan_voroniak@epam.com>
2025-01-19 15:04:40 +0200

    add logs

--- Offers/Program.cs
+++ Offers/Program.cs
@@ -1,9 +1,17 @@
+using Elastic.Ingest.Elasticsearch.DataStreams;
+using Elastic.Serilog.Sinks;
+using Elastic.Transport;
 using Offers.Options;
 using Offers.Services.Extensions;
 using Offers.Services.Services;
 using Offers.Services.Services.Interfaces;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigureLogging(builder.Configuration);
+builder.Host.UseSerilog();
+
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -27,3 +35,19 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
+
+
+void ConfigureLogging(IConfiguration configuration)
+{
+    Log.Logger = new LoggerConfiguration().Enrich.FromLogContext()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(new [] { new Uri(configuration["Elasticsearch:Url"] )}, opts =>
+        {
+            opts.DataStream = new DataStreamName("logs", "offers", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "development");
+        }, transport =>
+        {
+            transport.Authentication(new BasicAuthentication(configuration["Elasticsearch:Username"], configuration["Elasticsearch:Password"])); // Basic Auth
+        })
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+}
